cd6fd8adc30e5d1ea3a3243cfc6e3509c8963a9e
benitobosco <79128819+benitobosco@users.noreply.github.com>
2023-03-03 12:27:40 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.preprocessing import PowerTransformer
+
+
+csvfile = "X.csv"
+transformfile = "finalized_transform.sav"
+modelfile = "finalized_model.sav"
+
+
+st.title('Bandung House Price Predict')
+
+
+@st.cache_data
+def openalldata(csvfile, transformfile, modelfile):
+    file = pd.read_csv(csvfile)
+    transform = pickle.load(open(transformfile, 'rb'))
+    mlfile = pickle.load(open(modelfile, 'rb'))
+
+    return file, transform, mlfile
+
+fileloc, powertrans, model = openalldata(csvfile, transformfile, modelfile)
+
+
+def predict_price(location, bedroom, toilet, sqm_lot, sqm_living):
+    loc_index = np.where(fileloc.columns == location)[0][0]
+
+    x = np.zeros(len(fileloc.columns))
+    x[0] = bedroom
+    x[1] = toilet
+    x[2] = sqm_lot
+    x[3] = sqm_living
+    if loc_index >= 0:
+        x[loc_index] = 1
+    x = powertrans.transform(np.array(x).reshape(1, len(x)))
+    predict = model.predict(x)[0]
+    return predict
+
+# p = predict_price("Bandung Kota, Bandung",2,1,300,160)
+#
+# st.write(p)
+# st.text('Fixed width text')
+
+regions = []
+
+for i in fileloc.columns:
+    if "Bandung" in i:
+        regions.append(i)
+
+region = st.selectbox('Choose the region', regions)
+bedroom = st.number_input('Bedroom', 0)
+toilet = st.number_input('Toilet', 0)
+sqm_lot = st.number_input('Lot Area (m²)')
+sqm_living = st.number_input('Building Area (m²)')
+button = st.button('Make prediction')
+
+if button:
+    pricepred = predict_price(region, bedroom, toilet, sqm_lot, sqm_living)
+    st.success("Your house price is Rp{:,.2f}".format(pricepred))
+
